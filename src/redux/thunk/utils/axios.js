ef3085aa65fe5b4533881bfc5c28a58ab4d8030e
farrukh-ahm <farrukhahm@gmail.com>
2023-02-22 16:40:28 +0600

    url chagned

--- src/redux/thunk/utils/axios.js
+++ src/redux/thunk/utils/axios.js
@@ -1,7 +1,7 @@
-import axios from "./utils/axios";
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:9000",
+  baseURL: "http://localhost:8080",
 });
 
 export default axiosInstance;
